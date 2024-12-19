1cfa4b60639ccb59d3fda7253deca8c3d744f4d1
Bart Spaans <143680439+spaansba@users.noreply.github.com>
2024-12-19 10:56:08 +0100

    pwa install btton

--- src/app/_components/AppDownloadButton.tsx
+++ src/app/_components/AppDownloadButton.tsx
@@ -1,12 +1,42 @@
-import { usePWAInstall } from "react-use-pwa-install"
+import React, { useEffect, useState } from "react"
+import { Download } from "lucide-react"
 
-function AppDownloadButton() {
-  const install = usePWAInstall()
+interface BeforeInstallPromptEvent extends Event {
+  prompt: () => Promise<void>
+  userChoice: Promise<{ outcome: "accepted" | "dismissed" }>
+}
+
+const AppDownloadButton = () => {
+  const [deferredPrompt, setDeferredPrompt] = useState<any>(null)
+  const [isInstallable, setIsInstallable] = useState(false)
+  const [appInstalled, setAppInstalled] = useState(true)
+  useEffect(() => {
+    const handleBeforeInstallPrompt = (event: any) => {
+      event.preventDefault()
+      setDeferredPrompt(event)
+      setAppInstalled(!window.matchMedia("(display-mode: standalone)").matches)
+    }
+    window.addEventListener("beforeinstallprompt", handleBeforeInstallPrompt)
+
+    return () => {
+      window.removeEventListener("beforeinstallprompt", handleBeforeInstallPrompt)
+    }
+  }, [])
+
+  function handleInstallClick() {
+    if (!deferredPrompt) {
+      return
+    }
+    deferredPrompt.prompt()
+  }
   return (
-    <div>
-      <h1>My app</h1>
-      {install && <button onClick={install}>Install</button>}
-    </div>
+    <button
+      onClick={handleInstallClick}
+      className="flex items-center gap-2 px-4 py-2 text-white bg-blue-600 rounded-lg hover:bg-blue-700 transition-colors"
+    >
+      <Download className="w-5 h-5" />
+      <span>Install App</span>
+    </button>
   )
 }
 
